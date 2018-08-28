e49700cc79a86011227cee9ffcb727609af9e554
ali <bsef15m014@pucit.edu.pk>
2018-08-28 21:50:46 +0500

    Panaroma

--- Panaroma/src/main/java/com/google/android/cameraview/demo/MainActivity.java
+++ Panaroma/src/main/java/com/google/android/cameraview/demo/MainActivity.java
@@ -3,7 +3,9 @@ package com.google.android.cameraview.demo;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -19,17 +21,16 @@ public class MainActivity extends AppCompatActivity {
     int camPerm=0;
     int ReadPerm=0;
     int WritePerm=0;
+    private static final String TAG = "permission";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        if (checkSelfPermission(Manifest.permission.CAMERA)
-                    == PackageManager.PERMISSION_DENIED) {
-                requestPermissions(new String[]{Manifest.permission.CAMERA}, 1);
-            }
 
+        isWriteStoragePermissionGranted();
+        isReadStoragePermissionGranted();
 
 
         Button photoButton = (Button) findViewById(R.id.button_image);
@@ -108,54 +109,43 @@ public class MainActivity extends AppCompatActivity {
         } }
 
 */
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case 1: {
-                if (checkSelfPermission(Manifest.permission.CAMERA) ==
-                        PackageManager.PERMISSION_GRANTED){
 
-                    camPerm=1;
-                    // permission was granted
-                } else {
-                    // permission denied
-                    //Disable the functionality
-                    //that depends on this permission.
-                }
+    public  boolean isReadStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted1");
+                return true;
+            } else {
 
-
-
-                return;
+                Log.v(TAG,"Permission is revoked1");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 3);
+                return false;
             }
-
-            case 2: {
-                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
-                        PackageManager.PERMISSION_GRANTED){
-                    ReadPerm=1;
-                    // permission was granted
-                } else {
-                    // permission denied
-                    //Disable the functionality
-                    //that depends on this permission.
-                }
-
-                return;
-            }
-
-            case 3: {
-                if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
-                        PackageManager.PERMISSION_GRANTED){
-
-                    WritePerm=1;
-                    // permission was granted
-                } else {
-                    // permission denied
-                    //Disable the functionality
-                    //that depends on this permission.
-                }
-
-                return;
-            }
-
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted1");
+            return true;
         }
     }
+
+    public  boolean isWriteStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted2");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked2");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted2");
+            return true;
+        }
+    }
+
 }
--- Panaroma/src/main/java/com/google/android/cameraview/demo/SplashScreen.java
+++ Panaroma/src/main/java/com/google/android/cameraview/demo/SplashScreen.java
@@ -13,7 +13,7 @@ public class SplashScreen extends Activity {
 
     // Splash screen timer
     private static int SPLASH_TIME_OUT = 2000;
-    private static final String TAG = "permission";
+
     int camPerm=0;
     int ReadPerm=0;
     int WritePerm=0;
@@ -23,8 +23,12 @@ public class SplashScreen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        isWriteStoragePermissionGranted();
-        isReadStoragePermissionGranted();
+        if (checkSelfPermission(Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 1);
+        }
+
+
 
         new Handler().postDelayed(new Runnable() {
 
@@ -46,43 +50,7 @@ public class SplashScreen extends Activity {
         }, SPLASH_TIME_OUT);
 
         }
-    public  boolean isReadStoragePermissionGranted() {
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_GRANTED) {
-                Log.v(TAG,"Permission is granted1");
-                return true;
-            } else {
 
-                Log.v(TAG,"Permission is revoked1");
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 3);
-                return false;
-            }
-        }
-        else { //permission is automatically granted on sdk<23 upon installation
-            Log.v(TAG,"Permission is granted1");
-            return true;
-        }
-    }
-
-    public  boolean isWriteStoragePermissionGranted() {
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_GRANTED) {
-                Log.v(TAG,"Permission is granted2");
-                return true;
-            } else {
-
-                Log.v(TAG,"Permission is revoked2");
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
-                return false;
-            }
-        }
-        else { //permission is automatically granted on sdk<23 upon installation
-            Log.v(TAG,"Permission is granted2");
-            return true;
-        }
-    }
 
 
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
